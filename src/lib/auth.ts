c39cd66905c6b3882a29159941dc99962dcda4b4
Sam Chukwuemeka <engrsamchukwuemeka@gmail.com>
2025-09-08 16:25:38 +0000

    email verification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import prisma from '@/lib/prisma'
+import { sendmail } from './email'
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -8,6 +9,17 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  emailVerification:{
+    sendOnSignup: true,
+    autoSignAfterVerification: true,
+    async sendVerificationEmail({ user, url}) {
+      await sendmail({
+        to: user.email,
+        subject: 'Verify your email',
+        text: `Click here to verify your email: ${url}`,
+      })
+    }
   },
   user: {
     additionalFields: {
---
+++ src/lib/email.ts
@@ -0,0 +1,19 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+interface SendEnailVals {
+    to: string;
+    subject: string;
+    text: string;
+}
+
+export async function sendmail({ to, subject, text }: SendEnailVals) {
+    await resend.emails.send({
+        from: 'engrsamchukwuemeka@gmail.com',
+        to,
+        subject,
+        text,
+    });
+}
+    
